ce9221ddcfd294b534f73a193e8f537440c45ebd
ShiniYanada <ysthon@gmail.com>
2019-07-14 14:42:52 +0900

    productsControllerを作成

---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,48 @@
+class Api::V1::ProductsController < ApplicationController
+
+    def index 
+        @products = Product.all
+
+        render json: @products
+    end
+
+    def create 
+        @product = Product.new(product_params)
+
+        if @product.save 
+            render json: @product, status: :created
+        else
+            render json: product.errors, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        @product = Product.find_by(id: params[:id])
+        
+        render json: @product
+    end
+
+    def update
+        @product = Product.find_by(id: params[:id])
+        if @product.update(product_params)
+            render json: @product
+        else
+            render json: product.errors, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        product = Product.find_by(id: params[:id])
+        if product.destory
+            render json: status: 200
+        else
+            render json: status: unprocessable_entity
+        end
+    end
+
+    private 
+
+        def product_params 
+            params.permit(:name, :description, :price, :state, :category)
+        end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,11 @@ Rails.application.routes.draw do
       }
     end
   end
+
+  namespace :api do 
+    namespace :v1 do 
+      resources :products 
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
