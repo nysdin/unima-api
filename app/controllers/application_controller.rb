c629fbe5a47bd90cc3adc1a5fda8a2481b4951c7
ShiniYanada <ysthon@gmail.com>
2019-07-20 20:51:43 +0900

    configure_permitted_parametersでstrong parameterを操作

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::API
-        include Pundit
-        include DeviseTokenAuth::Concerns::SetUserByToken
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    include Pundit
+    include DeviseTokenAuth::Concerns::SetUserByToken
 
-        def pundit_user 
-                current_api_user 
+    def pundit_user 
+            current_api_user 
+    end
+    
+    protected
+
+        def configure_permitted_parameters
+            devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :nickname, 
+                                        :email, :img, :password, :password_confirmation])
+            devise_parameter_sanitizer.permit(:account_update, keys: [:name, :nickname, :email, :img])
         end
 end
