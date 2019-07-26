fd5bf7a37beabcf01f6cb0d41412d839b9186614
ShiniYanada <ysthon@gmail.com>
2019-07-26 22:18:54 +0900

    productとcategoryの関連を追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,9 @@ class Product < ApplicationRecord
     validates :name, presence: true, length: { in: 1..255 }
     validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
     validates :state, presence: true, inclusion: { in: %w(new almost_new almost_old old) }
+    validates :status, presence: true, inclusion: { in: %w(open trade close) }
 
     belongs_to :user
+    belongs_to :category
 
 end
