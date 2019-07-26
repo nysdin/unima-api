2348f6d2447459b6e8487f87e4dd87343c026e6a
ShiniYanada <ysthon@gmail.com>
2019-07-26 17:50:53 +0900

    categoriesテーブルを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_13_120259) do
+ActiveRecord::Schema.define(version: 2019_07_26_084703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "ancestry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name", null: false
