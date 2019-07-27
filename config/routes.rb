c1c5c56f77eeb76a7bd57b6b02b4dd7e1f8aeded
ShiniYanada <ysthon@gmail.com>
2019-07-27 20:49:12 +0900

    Add LikesController and routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
           post :complete
         end
       end
+      
+      resources :likes, only: [:create, :destroy]
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
