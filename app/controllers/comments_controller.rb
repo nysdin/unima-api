d6f3f8774d3e10c71b85f525530b60148188be8b
ShiniYanada <ysthon@gmail.com>
2019-07-31 21:45:56 +0900

    Add filter and comment_params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,15 @@
 class CommentsController < ApplicationController
+    before_action :authenticate_api_user!
     
     def create
     end
 
     def destroy
     end
+
+    private
+
+        def comment_params
+            params.permit(:contents)
+        end
 end
