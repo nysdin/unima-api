d3e42fc5160c47c3b1574b8d18e3408c1dc929b8
ShiniYanada <ysthon@gmail.com>
2019-07-31 21:42:06 +0900

    reloadでカウンターキャッシュを反映させた

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::LikesController < ApplicationController
     @product = Product.find_by(id: params[:product_id])
     if @product && !current_api_user.liking?(@product)
       current_api_user.like(@product)
-      render json: @product
+      render json: @product.reload
     else
       head :not_found
     end
