69c97dd6dd67e13ddcf99aa6a50afea06f1fa301
ShiniYanada <ysthon@gmail.com>
2019-08-27 14:12:24 +0900

    Add association of trade messages model

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,5 +11,6 @@ class Product < ApplicationRecord
     has_many :likes, dependent: :destroy
     has_many :liked_users, through: :likes, source: :user
     has_many :comments, dependent: :destroy
+    has_many :trade_messages, dependent: :destroy
 
 end
