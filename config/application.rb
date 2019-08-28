63fb74aa35a12960c980aa02425e83d5c38381e6
ShiniYanada <ysthon@gmail.com>
2019-08-28 15:06:57 +0900

    set default image

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,8 @@ module UnimaApi
     config.load_defaults 5.2
     config.time_zone = 'Asia/Tokyo'
 
+    config.asset_host = ENV['HOST_NAME']
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
