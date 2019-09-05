6d179e1991bbe13725b372c7fd81b3284eb61fdf
ShiniYanada <ysthon@gmail.com>
2019-09-05 17:28:55 +0900

    銀行口座のCRUD処理をするコントローラーとroutesを追加

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,8 @@ Rails.application.routes.draw do
         post '/bank', to: 'users#bank'
       end
 
-      resource :card
+      resource :card, except: [:create]
+      resource :bank_account, except: [:create]
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
