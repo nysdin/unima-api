3f298aec1f03c90941d0e2be052a1a640fc990a9
ShiniYanada <ysthon@gmail.com>
2019-09-05 21:22:55 +0900

    クレジットカード情報を取り出すshowアクションを追加

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -2,6 +2,23 @@ class Api::V1::CardsController < ApplicationController
     before_action :authenticate_api_user!
 
     def show
+        token = current_api_user.stripe_customer_id
+        if token.nil?
+            head :ok
+        else
+            begin
+                customer = Stripe::Customer.retrieve(token)
+                data = customer[:sources][:data][0]
+                last4 = data[:last4]
+                exp_month = data[:exp_month]
+                exp_year = data[:exp_year]
+                brand = data[:brand]
+                cregit = {last4: last4, exp_month: exp_month, exp_year: exp_year, brand: brand}
+                render json: cregit
+            rescue => e
+                head :bad_request and return
+            end
+        end
     end
 
     def update
