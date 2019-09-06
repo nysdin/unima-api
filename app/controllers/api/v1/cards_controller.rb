2c6fc58dfefdabd4bb0989bab7f22ed349410d25
ShiniYanada <ysthon@gmail.com>
2019-09-06 16:10:20 +0900

    口座情報をjsonで返す処理をrender_card()にまとめた

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -8,13 +8,7 @@ class Api::V1::CardsController < ApplicationController
         else
             begin
                 customer = Stripe::Customer.retrieve(customer_id)
-                data = customer[:sources][:data][0]
-                last4 = data[:last4]
-                exp_month = data[:exp_month]
-                exp_year = data[:exp_year]
-                brand = data[:brand]
-                cregit = {last4: last4, exp_month: exp_month, exp_year: exp_year, brand: brand}
-                render json: cregit
+                render_card(customer)
             rescue => e
                 head :bad_request and return
             end
@@ -29,20 +23,24 @@ class Api::V1::CardsController < ApplicationController
                     source: params[:stripe_cregit_token],
                     email: current_api_user.email,
                 })
-                current_api_user.stripe_customer_id = cutomer.id
+                if current_api_user.update_attributes(stripe_customer_id: cutomer.id)
+                    render_card(customer)
+                else
+                    head :unprocessable_entity
+                end
             rescue => e
                 head :bad_request and return
             end
         else
             begin
-                Stripe::Customer.update(customer_id, {
+                customer = Stripe::Customer.update(customer_id, {
                     source: params[:stripe_cregit_token],
                 })
+                render_card(customer)
             rescue => e
                 head :bad_request and return
             end
         end
-        head :ok
     end 
 
     def destroy
@@ -64,4 +62,17 @@ class Api::V1::CardsController < ApplicationController
             end
         end
     end
+
+    private
+
+        def render_card(customer)
+            customer = Stripe::Customer.retrieve(customer_id)
+            data = customer[:sources][:data][0]
+            last4 = data[:last4]
+            exp_month = data[:exp_month]
+            exp_year = data[:exp_year]
+            brand = data[:brand]
+            cregit = {last4: last4, exp_month: exp_month, exp_year: exp_year, brand: brand}
+            render json: cregit
+        end
 end
