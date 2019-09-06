cf9709b9eac497d44862137f15e3cdf2e0f9612c
ShiniYanada <ysthon@gmail.com>
2019-09-06 16:00:54 +0900

    update()に新しい口座情報をjsonで返す処理を追加した

--- app/controllers/api/v1/bank_accounts_controller.rb
+++ app/controllers/api/v1/bank_accounts_controller.rb
@@ -9,13 +9,7 @@ class Api::V1::BankAccountsController < ApplicationController
         else
             begin
                 account = Stripe::Account.retrieve(account_id)
-                data = account[:external_accounts][:data][0]
-                full_name = data[:account_holder_name]
-                bank_name = data[:bank_name]
-                last4 = data[:last4]
-                routing_number = data[:routing_number]
-                bank_data = { full_name: full_name, bank_name: bank_name, last4: last4, routing_number: routing_number}
-                render json: bank_data
+                render_bank_account(account)
             rescue => e
                 head :bad_request and return
             end
@@ -27,14 +21,26 @@ class Api::V1::BankAccountsController < ApplicationController
         head :forbidden and return if account_id.nil?
 
         begin 
-            Stripe::Account.update(account_id, {external_account: params[:stripe_bank_token]})
+            account = Stripe::Account.update(account_id, {external_account: params[:stripe_bank_token]})
+            render_bank_account(account)
         rescue => e
             head :bad_request and return
         end
-
-        head :ok
     end
 
     def destroy
     end
+
+    private
+
+        def render_bank_account(account)
+            data = account[:external_accounts][:data][0]
+            full_name = data[:account_holder_name]
+            bank_name = data[:bank_name]
+            last4 = data[:last4]
+            routing_number = data[:routing_number]
+            bank_data = { full_name: full_name, bank_name: bank_name, last4: last4, routing_number: routing_number}
+            render json: bank_data
+        end
+
 end
