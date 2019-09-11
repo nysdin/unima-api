a0cee5013948dc6de6986d911d1351f0f9a0b6dd
ShiniYanada <ysthon@gmail.com>
2019-09-11 17:44:00 +0900

    ユーザー情報が正しいかを検証するアクションを追加 validate_account()

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,14 @@
 class Api::V1::UsersController < ApplicationController
-    before_action :authenticate_api_user!
+    before_action :authenticate_api_user!, except: [:validate_account]
+
+    def validate_account
+        @user = User.new(user_params)
+        if @user.valid?
+            head :ok
+        else
+            render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity	
+        end
+    end
 
     def sell
         @products = current_api_user.sell_products
@@ -28,5 +37,11 @@ class Api::V1::UsersController < ApplicationController
 
         head :ok
     end
+
+    private
+
+        def user_params
+            params.permit(:name, :email, :password, :password_confirmation)
+        end
 
 end
