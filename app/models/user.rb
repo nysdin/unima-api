b88774c8a67452a268b038ed6239f091e84a37ab
ShiniYanada <ysthon@gmail.com>
2019-09-13 11:00:01 +0900

    presence:true maximum: 15をnameカラムに追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,8 @@ class User < ActiveRecord::Base
   has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
   has_many :followers, through: :passive_relationships, source: :follower
 
+  validates :name, presence: true, length: { maximum: 15 }
+
   def like(product)
     likes.create(product_id: product.id)
   end
