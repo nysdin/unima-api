589d0f441c20cca95667f6a53a5907fe6916b68c
ShiniYanada <ysthon@gmail.com>
2019-09-17 21:18:57 +0900

    コネクション時の認証を追加

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,27 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      params = request.query_parameters()
+
+      access_token = params["access-token"]
+      uid = params["uid"]
+      client = params["client"]
+
+      self.current_user = find_verified_user(access_token, uid, client)
+    end
+
+    private
+
+      def find_verified_user(token, uid, client_id)
+        user = User.find_by(email: uid)
+
+        if user && user.valid_token?(token, client_id)
+          user
+        else
+          reject_unauthorized_connection
+        end
+      end
   end
 end
