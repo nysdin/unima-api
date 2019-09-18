2bf96ce49a7f3f48274805526002f2fd1ee81a13
ShiniYanada <ysthon@gmail.com>
2019-09-18 15:38:09 +0900

    関連を追加

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,6 +1,7 @@
 class Notification < ApplicationRecord
-  belongs_to :user
+  belongs_to :sender, class_name: 'User'
+  belongs_to :recipient, class_name: 'User'
   belongs_to :product
 
-  validates :content, presence: true
+  validates :action, presence: true
 end
