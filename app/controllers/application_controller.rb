b20fff164525ae01c6846c3d25b81710cec36358
ShiniYanada <ysthon@gmail.com>
2019-09-20 21:58:50 +0900

    Include Pagy::Backend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::API
     before_action :configure_permitted_parameters, if: :devise_controller?
     include Pundit
     include DeviseTokenAuth::Concerns::SetUserByToken
+    include Pagy::Backend
 
     def pundit_user 
             current_api_user 
