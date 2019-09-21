1b4b74f4d5196d3782581b01576e8d1a8ec21777
ShiniYanada <ysthon@gmail.com>
2019-09-21 15:46:28 +0900

    typoを直した

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,17 @@
 CarrierWave.configure do |config|
 
-    if Rails.env.productio?
+    if Rails.env.production?
         config.storage = :fog
         config.fog_provider = 'fog/aws'
         config.fog_directory = ENV['AWS_S3_BUCKET']
         config.fog_credentials = {
-            provider : 'AWS',
+            provider: 'AWS',
             aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
             aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
             region: ENV['AWS_REGION'],
             path_style: true
         }
+        config.asset_host = ENV['HOST_NAME']
     else
         config.storage :file
         config.asset_host = ENV['HOST_NAME']
