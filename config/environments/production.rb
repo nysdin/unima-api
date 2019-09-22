56773e6ab492a6e4d9808cd7de42f129838be722
ShiniYanada <ysthon@gmail.com>
2019-09-22 09:43:17 +0900

    Add action_cable.allowed_request_origins

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,4 +85,5 @@ Rails.application.configure do
 
   #action cable
   config.action_cable.url = 'wss://unima-rails-api.herokuapp.com:3000/cable'
+  config.action_cable.allowed_request_origins = ["https://unima.netlify.com"]
 end
